af7df624a84bb328f7ebb6e2be272b3fa7a69719
artstalker <groshevoy@gmail.com>
2013-03-10 14:56:33 +0200

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def create
@@ -51,13 +52,6 @@ class UsersController < ApplicationController
   end
 
   private
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def unsigned_in_user
       redirect_to root_path  unless !signed_in?
